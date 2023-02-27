a5461a4548d407aec4520ad9c3ba8d940681f710
BrunoBenunes <brunobenuness@hotmail.com>
2023-02-27 19:34:54 -0300

    Realizado a etapa 3 do exercicio 1 - Retornar o maior valor entre 3 numeros

--- fundamentos/secao-03-introducao-a-JavaScript/dia-01-javascript-primeiros-passos/Exercicio-Pratica-01.js
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-01-javascript-primeiros-passos/Exercicio-Pratica-01.js
@@ -1,5 +1,6 @@
 let a = 10
 let b = 5
+let c = 20
 const adicao = a + b;
 const subtracao = a - b;
 const multiplicacao = a * b;
@@ -9,7 +10,14 @@ console.log(adicao,subtracao,multiplicacao,divisao,modulo);
 
 if ( a > b) {
     console.log('O numero ',a,' é maior que ',b);
-}
-else {
+} else {
     console.log('O numero ',b,' é maior que ',a);
+}
+
+if ( a > b && a > c){
+    console.log('Entre ',a,b,c,' O maior numero é o ',a);
+} else if (b > a && b > c) {
+    console.log('Entre ',a,b,c,' O maior numero é o ',b);
+} else if (c > a && c > b) {
+    console.log('Entre ',a,b,c,' O maior numero é o ',c);
 }
