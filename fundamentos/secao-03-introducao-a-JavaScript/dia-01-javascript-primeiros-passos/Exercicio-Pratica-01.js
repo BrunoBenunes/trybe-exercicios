7dec81bfb0b043180efffb280704492ff9de5249
BrunoBenunes <brunobenuness@hotmail.com>
2023-02-27 19:15:40 -0300

    Primeiro Passo do Exercicio 1 - Na Pratica - operaçãoes

---
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-01-javascript-primeiros-passos/Exercicio-Pratica-01.js
@@ -0,0 +1,8 @@
+let a = 10
+let b = 5
+const adicao = a + b;
+const subtracao = a - b;
+const multiplicacao = a * b;
+const divisao = a / b;
+const modulo = a % b;
+console.log(adicao,subtracao,multiplicacao,divisao,modulo);
