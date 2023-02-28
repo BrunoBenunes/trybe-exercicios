89993a27497b7259b7eefbab593531da24c21a04
BrunoBenunes <brunobenuness@hotmail.com>
2023-02-27 22:51:48 -0300

    Realizado o Exercicio 6 - Na Pratica - Há um impar entre nós

---
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-01-javascript-primeiros-passos/Exercicio-Pratica-06.js
@@ -0,0 +1,10 @@
+// Há Um impar entre nós
+let num1 = 6
+let num2 = 5
+let num3 = 10
+if (num1 % 2 != 0 || num2 % 2 != 0 || num3 % 2 != 0) {
+    console.log('True')
+}
+else {
+    console.log('False')
+}
