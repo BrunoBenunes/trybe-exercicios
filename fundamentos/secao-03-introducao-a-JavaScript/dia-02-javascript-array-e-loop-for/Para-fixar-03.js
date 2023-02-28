01099a2b3698d0dd4f7eae23ed11fbeb80cb1616
BrunoBenunes <brunobenuness@hotmail.com>
2023-02-28 15:32:05 -0300

    Realizado o Exercicio 3 do Para Fixar - Adicionando um valor ao final do array

---
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-02-javascript-array-e-loop-for/Para-fixar-03.js
@@ -0,0 +1,11 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato')
+
+console.log(menu);
+
+// Ou assim:
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let addMenu = 'Contato'
+const itenAddMenu = menu2.push(addMenu)
+
+console.log(menu2);
