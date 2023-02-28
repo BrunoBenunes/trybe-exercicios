218870bf332c7c67ff51252440ceecd1ccd5b722
BrunoBenunes <brunobenuness@hotmail.com>
2023-02-28 15:28:17 -0300

    Realizado o Exercicio 2 do Para Fixar - Retornando o Index de um valor no Array menu

---
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-02-javascript-array-e-loop-for/Para-fixar-02.js
@@ -0,0 +1,12 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+// Ou assim:
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let itenSrcMenu = 'Portfólio'
+const srcMenu = menu.indexOf(itenSrcMenu)
+let indexOfPortfolio2 = srcMenu;
+
+console.log(indexOfPortfolio);
