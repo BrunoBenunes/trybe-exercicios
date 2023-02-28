ee06c93c39cefc3d21c177770ebcad86bc3d1f2a
BrunoBenunes <brunobenuness@hotmail.com>
2023-02-28 18:40:06 -0300

    Realizado a etapda 6 do Exercicio 1- Na pratica - Descobrindo  quantos valores ímpares existem na lista numbers

--- fundamentos/secao-03-introducao-a-JavaScript/dia-02-javascript-array-e-loop-for/Exercicio-Pratica-01.js
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-02-javascript-array-e-loop-for/Exercicio-Pratica-01.js
@@ -1,4 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// Somando e Descobrindo a media aritimetica
+
 let sumNumbers = 0;
 
 for  (let index = 0; index < numbers.length; index += 1) {
@@ -9,17 +11,35 @@ for  (let index = 0; index < numbers.length; index += 1) {
 const arithmeticAverangeNumbers = sumNumbers/numbers.length;
 console.log('A soma de todos os numeros do array é: ' + sumNumbers,'\n E a Média Aritimetica da lista numbers é :', + arithmeticAverangeNumbers);
 
+// Descobrindo se o ultimo elemento e maior que 20
+
 if (numbers[numbers.length - 1] > 20) {
     console.log('O ultimo elemento da lista numbers é maior que 20.');
 } else {
     console.log('O ultimo elemento da lista numbers é menor ou igual a 20');
 };
 
+// Descobrindo o Maior Valor contido na lista numbers
+
 let highNumber = 0
 
 for (let index = 0; index < numbers.length ; index += 1) {
     if (numbers[index] > highNumber){
-        highNumber = numbers[index]
+        highNumber = numbers[index];
     }
 }
 console.log('O maior valor contido na lista numbers é: ' + highNumber)
+
+// Descobrindo quantos valores ímpares existem na lista numbers
+
+let oddCount = 0;
+let oddElement = []
+
+for (let index = 0; index < numbers.length ; index += 1) {
+    if (! (numbers[index] % 2 === 0)) {
+        oddCount += 1;
+        oddElement.push(numbers[index])
+    } 
+}
+console.log(oddCount,oddElement);
+
