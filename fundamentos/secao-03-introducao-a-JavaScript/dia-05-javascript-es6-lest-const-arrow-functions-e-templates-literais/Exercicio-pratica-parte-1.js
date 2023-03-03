125715fe674a7da02bd5ce137e7363ceff2bab92
BrunoBenunes <brunobenuness@hotmail.com>
2023-03-03 18:20:31 -0300

    Realizado a etapa 3 e 4 do Exercicio na Pratica - Parte 1

--- fundamentos/secao-03-introducao-a-JavaScript/dia-05-javascript-es6-lest-const-arrow-functions-e-templates-literais/Exercicio-pratica-parte-1.js
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-05-javascript-es6-lest-const-arrow-functions-e-templates-literais/Exercicio-pratica-parte-1.js
@@ -8,12 +8,35 @@ imprimeIdade(30)
 
 // 2-
 
-  const pessoa = {
-    nome: 'Henri',
-    idade: 20
-  }
-  pessoa.nome = 'Luna'
-  pessoa.idade = 19
+const pessoa = {
+nome: 'Henri',
+idade: 20
+}
+pessoa.nome = 'Luna'
+pessoa.idade = 19
 
-  console.log('Nome:', pessoa.nome);
-  console.log('Idade:', pessoa.idade);
+console.log('Nome:', pessoa.nome);
+console.log('Idade:', pessoa.idade);
+
+// 3- 
+
+let favoriteFood = 'Lasanha';
+
+favoriteFood = 'Hambúrguer';
+console.log(favoriteFood);
+
+// 4- 
+const name = 'Adriana';
+const lastName = 'Soares';
+
+console.log(`Olá ${name} ${lastName} !`);
+
+function soma(a,b) {
+  let resultado = a + b;
+  return resultado;
+}
+
+let a = 3;
+let b = 5;
+
+console.log(`O resultado da soma de ${a} +  ${b} é: ${soma(a,b)}`);
