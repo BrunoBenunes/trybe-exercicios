7eb229a5c7bc7a239d54f90ff76dbbede69e723f
BrunoBenunes <brunobenuness@hotmail.com>
2023-03-03 18:26:27 -0300

    Realizado a etapa 5 - 6 - 7 do Exericio na Pratica - Parte 1

--- fundamentos/secao-03-introducao-a-JavaScript/dia-05-javascript-es6-lest-const-arrow-functions-e-templates-literais/Exercicio-pratica-parte-1.js
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-05-javascript-es6-lest-const-arrow-functions-e-templates-literais/Exercicio-pratica-parte-1.js
@@ -40,3 +40,23 @@ let a = 3;
 let b = 5;
 
 console.log(`O resultado da soma de ${a} +  ${b} é: ${soma(a,b)}`);
+
+// 5 ----------------------------------
+const numeroAleatorio = () => {
+  return Math.random()
+}
+console.log(numeroAleatorio());
+
+// 6 ----------------------------------
+const hello = (nome) => {
+  return `Olá, ${nome}!`
+}
+let nome = 'Ivan';
+console.log(hello(nome));
+
+// 7 ---------------------------------- 
+const nomeCompleto = (nome, sobrenome) => {
+  return `${nome} ${sobrenome}`
+}
+let sobrenome = 'Pires'
+console.log(nomeCompleto(nome, sobrenome));
