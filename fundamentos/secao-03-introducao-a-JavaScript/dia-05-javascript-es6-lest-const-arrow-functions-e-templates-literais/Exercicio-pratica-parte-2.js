1e6bca88238c60f094eb1bb4b1469477e0a9b900
BrunoBenunes <brunobenuness@hotmail.com>
2023-03-03 18:46:27 -0300

    Realizado o Exercicio 1 da Parte 2 -  Mais pratica - Funcção que liga e desliga um carro

---
+++ fundamentos/secao-03-introducao-a-JavaScript/dia-05-javascript-es6-lest-const-arrow-functions-e-templates-literais/Exercicio-pratica-parte-2.js
@@ -0,0 +1,13 @@
+const car = {
+  status: 'on'
+}
+
+const startStopCar = (power) => {
+  if (car.status === power) {
+    car.status = 'off'
+  } else {
+    car.status = 'on'
+  }
+}
+startStopCar('on')
+console.log(car)
